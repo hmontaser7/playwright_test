0946609f08385f3b99dbbfc54f79ae4ee69384ca
Hesham Montaser <h.montaser@moddakir.com>
2025-10-28 16:04:54 +0300

    publishing Login APi Repo

---
+++ tests/login.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect, request } from '@playwright/test';
+
+const baseURL= "https://revamp-auth.moddakir.com";
+
+
+test('LoginAPI', async({request})=>{
+    const payload ={     
+  "email": "hmontaser00@moddakir.com",
+  "username": "RVaR2HxUiVMEEc26WZvv",
+  "password": "Abc@@12345"
+    }
+
+const response = await request.post(`${baseURL}/api/auth/public/login`,{
+
+data:payload
+});
+
+let responseBody = await response.json();
+console.log(responseBody);
+expect(response.status()).toBe(200);
+expect(responseBody.data.student.phone).toEqual("+201054777899");
+let accessToken = responseBody.data.accessToken;
+console.log("accessToken=" + accessToken);
+
+})
